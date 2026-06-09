f5a650b50ba52192b4b18fe1ec84fe5f29c4d195
m9ts <mat3us.gois@gmail.com>
2026-06-09 15:40:33 -0300

    fix: adicionar método findByValorNota no SlotNotaRepository

--- src/main/java/com/sistemabankcoxinha/repository/MovimentacaoRepository.java
+++ src/main/java/com/sistemabankcoxinha/repository/MovimentacaoRepository.java
@@ -1,8 +1,10 @@
 package com.sistemabankcoxinha.repository;
 
+import com.sistemabankcoxinha.model.Cliente;
 import com.sistemabankcoxinha.model.Movimentacao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface MovimentacaoRepository extends JpaRepository<Movimentacao, Long> {
-
+    Optional<Movimentacao> findFirstByClienteOrderByDataHoraDesc(Cliente cliente);
 }
--- src/main/java/com/sistemabankcoxinha/repository/SlotNotaRepository.java
+++ src/main/java/com/sistemabankcoxinha/repository/SlotNotaRepository.java
@@ -2,9 +2,8 @@ package com.sistemabankcoxinha.repository;
 
 import com.sistemabankcoxinha.model.SlotNota;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
 public interface SlotNotaRepository extends JpaRepository<SlotNota, Long> {
-
+    Optional<SlotNota> findByValorNota(Integer valorNota);
 }
