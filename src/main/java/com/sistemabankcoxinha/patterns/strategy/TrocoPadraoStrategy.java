1fb62da339746a052f88f3c062646e76cba55b76
m9ts <mat3us.gois@gmail.com>
2026-06-26 18:23:08 -0300

    fix: remoção de import inutilizado

--- src/main/java/com/sistemabankcoxinha/patterns/strategy/TrocoPadraoStrategy.java
+++ src/main/java/com/sistemabankcoxinha/patterns/strategy/TrocoPadraoStrategy.java
@@ -2,7 +2,6 @@ package com.sistemabankcoxinha.patterns.strategy;
 
 import com.sistemabankcoxinha.model.SlotNota;
 
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
